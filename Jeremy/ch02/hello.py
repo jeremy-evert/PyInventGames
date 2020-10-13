c9df50744a3ef532efaf66a02e9646f8ab4301bd
Jeremy Evert FIONA <jeremy.evert@swosu.edu>
2020-10-13 09:58:46 -0500

    added more stuff.

---
+++ Jeremy/ch02/hello.py
@@ -0,0 +1,7 @@
+print('begin program')
+# This program says hello and asks for my name.
+print('Hello, World!')
+print('What is your name?')
+myName = input()
+print('It is good to meet you, ' + myName)
+print('end program')
---
+++ Jeremy/ch04/jokes.py
@@ -0,0 +1,22 @@
+print('Begin program.')
+
+print('What do you get when you corss a snowman with a vampire?')
+input()
+print('Frostbite!\n\n')
+
+print("What do dentists call an astronaut's cavity?")
+input()
+print('A black hole!\n\n')
+
+print('Knock knock.')
+input()
+print("Who's there?")
+input()
+print('Interrupting cow.')
+input()
+print('Interrupting cow wh', end= '')
+print('-MOO!')
+
+
+
+print('program complete.')
